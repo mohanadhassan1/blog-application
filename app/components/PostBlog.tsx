b4ac672d5d73f62d21fd4e06aee020376bcd7b63
mohanad <mohanadhassan246@gmail.com>
2024-06-22 03:05:50 +0300

    api and localstorage

--- app/components/PostBlog.tsx
+++ app/components/PostBlog.tsx
@@ -6,35 +6,24 @@ import Link from "next/link";
 
 import { useSelector, useDispatch } from "react-redux";
 import { useEffect } from "react";
-import { RootState, AppDispatch } from "../../store";
 
+import { RootState, AppDispatch } from "../../store";
 import { fetchPosts } from "../../store/slice/blogSlice";
-// import { BlogPost } from "../../store/slice/blogSlice";
 import { loadPostsFromLocalStorage } from "../../store/slice/createSlice";
 
 import { MutatingDots } from "react-loader-spinner";
 
 import { BlogCard } from "../lib/interfaces";
 
-
-
 export function PostBlog() {
-
   const dispatch = useDispatch<AppDispatch>();
   const blogPosts = useSelector((state: RootState) => state.blog.posts);
   const localPosts = useSelector((state: RootState) => state.localPosts.posts);
   const status = useSelector((state: RootState) => state.blog.status);
   const error = useSelector((state: RootState) => state.blog.error);
 
-  // useEffect(() => {
-  //   dispatch(fetchPosts());
-  // }, [dispatch]);
-
-  // const localPosts: BlogCard[] = JSON.parse(localStorage.getItem('posts') || '[]');
-  // const localPosts: BlogPost[] = JSON.parse(localStorage.getItem('posts') || '[]'); 
-  
   useEffect(() => {
-    if (status === 'idle') {
+    if (status === "idle") {
       dispatch(fetchPosts());
     }
   }, [dispatch, status]);
@@ -42,7 +31,6 @@ export function PostBlog() {
   useEffect(() => {
     dispatch(loadPostsFromLocalStorage());
   }, [dispatch]);
-
 
   return (
     <>
@@ -65,17 +53,18 @@ export function PostBlog() {
       <div className="grid grid-cols-1  md:grid-cols-2 mt-5 gap-5">
         {status === "failed" && <p>Error: {error}</p>}
         {status === "succeeded" &&
-          // blogPosts.map((post: any) => (
-          // [...blogPosts, ...localPosts].map((post: BlogCard) => (
-          // [...blogPosts].map((post: BlogCard) => (
-            localPosts.map((post: any) => (
+          [...localPosts, ...blogPosts].map((post: BlogCard) => (
             <Card key={post.id}>
-              <CardContent className="mt-5">
-                <h3 className="text-lg line-clamp-2 font-bold">{post.title}</h3>
-                <p className="line-clamp-3 text-sm mt-2 text-gray-600 dark:text-gray-300">
-                  {post.body}
-                </p>
-                <Button asChild className="w-full mt-7">
+              <CardContent className="mt-5 flex flex-col justify-between h-full">
+                <div>
+                  <h3 className="text-lg line-clamp-2 font-bold">
+                    {post.title}
+                  </h3>
+                  <p className="line-clamp-3 text-sm mt-2 text-gray-600 dark:text-gray-300">
+                    {post.body}
+                  </p>
+                </div>
+                <Button asChild className="w-full my-2">
                   <Link href={`/blog/${post.id}`}>Read More</Link>
                 </Button>
               </CardContent>
@@ -84,4 +73,4 @@ export function PostBlog() {
       </div>
     </>
   );
-};
+}
